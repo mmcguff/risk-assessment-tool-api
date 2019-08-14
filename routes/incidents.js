abbfc9e5424ec9eddbb48052aba6731823ec944a
Matthew McGuff <matthew.mcguff@walmart.com>
2019-08-13 22:42:12 -0500

    add routes for data

--- routes/incidents.js
+++ routes/incidents.js
@@ -7,7 +7,6 @@ const targetIncidents = [ 'Fire', 'Flood', 'Drought', 'Hurricane', 'Tornado', 'E
 const { url, configs, pool } = require('../configs/dbConfig');
 const validationMiddleware = require('../middleware/validationMiddleware');
 
-
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
 
@@ -31,6 +30,34 @@ router.get('/disasters/:state/:zip', validationMiddleware, async (req, res) => {
   const state = await req.params.state.toUpperCase();
   const zip = await req.params.zip;
   const queryString = `select * from disasterdec where state = '${state}' and AFZIPCODES like '%${zip}%';`;
+
+  pool.getConnection(function(err, connection) {
+    connection.query(queryString, async (err, result) => {
+        connection.release();
+        if (err) throw err;
+        res.send(result);
+      });
+    });
+})
+
+router.get('/demographics/:zip', validationMiddleware, async (req, res) => {
+
+  const zip = await req.params.zip;
+  const queryString = `select * from PopulationZip where zip = ${zip} order by minage, gender;`;
+
+  pool.getConnection(function(err, connection) {
+    connection.query(queryString, async (err, result) => {
+        connection.release();
+        if (err) throw err;
+        res.send(result);
+      });
+    });
+})
+
+router.get('/zip-data/:zip', validationMiddleware, async (req, res) => {
+
+  const zip = await req.params.zip;
+  const queryString = `select * from Zipcodes where zip = ${zip};`;
 
   pool.getConnection(function(err, connection) {
     connection.query(queryString, async (err, result) => {
