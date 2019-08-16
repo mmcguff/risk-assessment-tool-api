1ee5aa24805204c827cca9f47960709d5e7bdac4
Matthew McGuff <matthew.mcguff@walmart.com>
2019-08-16 06:16:11 -0500

    final changes before merge

--- routes/incidents.js
+++ routes/incidents.js
@@ -2,28 +2,11 @@ const express = require('express');
 const router = express.Router();
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
-const utils = require('./helper/utils');
-const targetIncidents = [ 'Fire', 'Flood', 'Drought', 'Hurricane', 'Tornado', 'Earthquake', 'Snow' ];
-const { url, configs, pool } = require('../configs/dbConfig');
+const { pool } = require('../configs/dbConfig');
 const validationMiddleware = require('../middleware/validationMiddleware');
 
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
-
-router.get('/fema/:state', validationMiddleware, async (req, res) => {
-
-    MongoClient.connect(url, function(err, db) {
-        if (err) throw err;
-        const dbo = db.db(configs.db);
-        var query = { state: req.params.state.toUpperCase(), incidentType: {$in: targetIncidents} };
-        dbo.collection("fema").find(query).toArray(function(err, body) {
-          if (err) throw err;
-            res.send(utils.transformFEMABody(body));
-          db.close();
-        });
-      });
-
-});
 
 router.get('/disasters/:state/:zip', validationMiddleware, async (req, res) => {
 
