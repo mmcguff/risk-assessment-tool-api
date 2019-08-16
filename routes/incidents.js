218acc6e99babe86072d3d2498a0f2e0f152da8b
Matthew McGuff <matthew.mcguff@walmart.com>
2019-08-16 06:39:02 -0500

    closing connection after response.

--- routes/incidents.js
+++ routes/incidents.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 const { pool } = require('../configs/dbConfig');
 const validationMiddleware = require('../middleware/validationMiddleware');
@@ -16,9 +15,9 @@ router.get('/disasters/:state/:zip', validationMiddleware, async (req, res) => {
 
   pool.getConnection(function(err, connection) {
     connection.query(queryString, async (err, result) => {
-        connection.release();
         if (err) throw err;
-        res.send(result);
+        await res.send(result);
+        await connection.release();
       });
     });
 })
@@ -30,9 +29,9 @@ router.get('/demographics/:zip', validationMiddleware, async (req, res) => {
 
   pool.getConnection(function(err, connection) {
     connection.query(queryString, async (err, result) => {
-        connection.release();
         if (err) throw err;
-        res.send(result);
+        await res.send(result);
+        await connection.release();
       });
     });
 })
@@ -44,9 +43,9 @@ router.get('/zip-data/:zip', validationMiddleware, async (req, res) => {
 
   pool.getConnection(function(err, connection) {
     connection.query(queryString, async (err, result) => {
-        connection.release();
         if (err) throw err;
-        res.send(result);
+      await  res.send(result);
+      await  connection.release();
       });
     });
 })
